a9256eb0e7a17cb1e4c1885c46b2dd6b0d615913
gaoshuai <gaoshuai@qnzsai.com>
2021-09-13 16:32:53 +0800

    UserController 代码修改

--- controllers/User.go
+++ controllers/User.go
@@ -5,21 +5,22 @@ import (
 	"web/util"
 )
 
-type UserController struct{
+type UserController struct {
 	BaseController
 }
-func (this *UserController) Login(){
-	var paramUtil  = util.ParamUtil{}
+
+func (this *UserController) Login() {
+	var paramUtil = util.ParamUtil{}
 	var paramsMap = map[string]map[string]string{
-		"password":{"from":"password","required":"1","name":"密码","default":""},
-		"user_name":{"from":"user_name","required":"1","name":"用户名","default":""},
+		"password":  {"from": "password", "required": "1", "name": "密码", "default": ""},
+		"user_name": {"from": "user_name", "required": "1", "name": "用户名", "default": ""},
 	}
-	var params,err = paramUtil.GetParams(paramsMap, this.Ctx)
+	var params, err = paramUtil.GetParams(paramsMap, this.Ctx)
 	msg := "ok"
-	if(!err){
-		msg = paramUtil.Error;
+	if !err {
+		msg = paramUtil.Error.Error()
 	}
-	temp,_ := json.Marshal(params)
+	temp, _ := json.Marshal(params)
 	var result = string(temp[:])
-	this.ExitJson(0,msg,result,"")
+	this.ExitJson(0, msg, result, "")
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,16 @@ module web
 
 go 1.14
 
-require github.com/astaxie/beego v1.12.1
+require github.com/astaxie/beego v1.12.3
 
 require (
+	github.com/OwnLocal/goes v1.0.0 // indirect
+	github.com/PuerkitoBio/goquery v1.7.1
+	github.com/axgle/mahonia v0.0.0-20180208002826-3358181d7394
 	github.com/shiena/ansicolor v0.0.0-20200904210342-c7312218db18 // indirect
+	github.com/siddontang/go v0.0.0-20180604090527-bdc77568d726 // indirect
+	github.com/siddontang/ledisdb v0.0.0-20181029004158-becf5f38d373 // indirect
 	github.com/smartystreets/goconvey v1.6.4
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/tools v0.0.0-20200117065230-39095c1d176c // indirect
 )
